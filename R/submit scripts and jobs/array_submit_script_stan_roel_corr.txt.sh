0c93ee6439fdc93c80beef7d11627aa5d5ee14e4
van Klink <rk59zeqi@idiv.de>
2021-09-30 10:03:49 +0200

    changed nr cores to 1
    added emails
    changed script path

--- R/submit scripts and jobs/array_submit_script_stan_roel_corr.txt.sh
+++ R/submit scripts and jobs/array_submit_script_stan_roel_corr.txt.sh
@@ -2,14 +2,15 @@
 
 #SBATCH --time 0-06:00:00
 
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 
 #SBATCH --mem-per-cpu=1G
 
 #SBATCH --job-name=Roel_taxa_cors
 
 #SBATCH -o /work/%u/%x-%A-%a.txt
-
+# send mail with start, end or abort for this job:
+#SBATCH --mail-type=ALL     
 #SBATCH --array=1-71
 
 echo "processing chunk $SLURM_ARRAY_TASK_ID ..."
@@ -19,4 +20,4 @@ module load foss/2020b R/4.0.4-2
 mkdir -p /work/$USER/$SLURM_JOB_NAME/$SLURM_ARRAY_JOB_ID
 cd /work/$USER/$SLURM_JOB_NAME/$SLURM_ARRAY_JOB_ID
 
-Rscript /data/idiv_ess/Roel/simple_correlations_brms.R
+Rscript /data/idiv_chase/vanKlink/simple_correlations_brms.R
